975219a8daa3c37e14e225ae68f636ab23fe0aa6
DeepuSahni <sahni007@gmail.com>
2017-12-03 10:52:40 +1100

    Added java client and server projects

---
+++ deepak-projects/deepak-server/src/main/java/server/controller/AddressController.java
@@ -0,0 +1,25 @@
+package server.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.*;
+import server.representation.Address;
+import server.builder.AddressBuilder;
+import server.util.ConversionHelper;
+
+import java.util.UUID;
+
+@RestController
+@EnableAutoConfiguration
+public class AddressController {
+
+    @RequestMapping("/getAddress")
+    public String getAddress() {
+        Address addressToSend =  AddressBuilder.buildAddressWithServerSpecs("Rob " + UUID.randomUUID().toString().substring(0,5));
+        return ConversionHelper.asJsonStringOrBlank(addressToSend);
+    }
+}
+
+
+
+
+
